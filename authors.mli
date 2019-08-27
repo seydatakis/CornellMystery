f50e59e99f09be34ab36057078c9049430da4d2e
seydatakis <42819630+seydatakis@users.noreply.github.com>
2019-08-27 11:48:19 -0400

    Delete authors.mli

--- authors.mli
+++
@@ -1,8 +0,0 @@
-(**
-   Metadata about this submission.
-   @author TODO All author names and NetIDs. 
-*)
-
-(** [hours_worked] is a list of the number of hours each team member
-    worked on this assignment. *)
-val hours_worked : int list
